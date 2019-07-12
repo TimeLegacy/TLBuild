59d0544101fb4a707d34d5eb8d239538558c5e26
Shaun Agostinho <shaunagostinho@gmail.com>
2019-07-11 17:54:22 -0700

    permission levels done. and player storage has been started

--- src/main/java/net/verade/leveling/events/PlayerEvents.java
+++ src/main/java/net/verade/leveling/events/PlayerEvents.java
@@ -1,7 +1,6 @@
 package net.verade.leveling.events;
 
 import net.verade.leveling.LevelPermissions;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,18 +13,6 @@ public class PlayerEvents implements Listener {
     Player player = event.getPlayer();
 
     LevelPermissions.setPlayerPermissions(player);
-
-    if (player.hasPermission("test.permission1")) {
-      Bukkit.broadcastMessage("yes 1");
-    }
-
-    if (player.hasPermission("test.permission4")) {
-      Bukkit.broadcastMessage("yes 2");
-    }
-
-    if (player.hasPermission("test.permission7")) {
-      Bukkit.broadcastMessage("yes 3");
-    }
   }
 
 }
