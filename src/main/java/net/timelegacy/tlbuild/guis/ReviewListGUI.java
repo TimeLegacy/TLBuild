7bb3cb56c014490aab0c994e84bbc56b934ef88e
Steven Conley <shadowmastergaming23@gmail.com>
2019-07-17 01:25:55 -0500

    Removed unused import.

--- src/main/java/net/timelegacy/tlbuild/guis/ReviewListGUI.java
+++ src/main/java/net/timelegacy/tlbuild/guis/ReviewListGUI.java
@@ -10,7 +10,6 @@ import net.timelegacy.tlcore.utils.MenuUtils;
 import net.timelegacy.tlcore.utils.MessageUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
